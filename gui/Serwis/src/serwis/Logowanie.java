c705a62acd271939e3b29b4ca6d3868cfe262437
Kacper <walekkacper@gmail.com>
2017-03-27 11:33:37 +0200

    Dodano graficzny interfejs użytkownika.

---
+++ gui/Serwis/src/serwis/Logowanie.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serwis;
+
+/**
+ *
+ * @author student
+ */
+public class Logowanie {
+    Logowanie(){
+        new GuiLogowanie().setVisible(true);
+    }
+    public static void main(String args[]) {
+        
+    }
+}
